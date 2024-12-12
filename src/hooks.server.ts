e7267e72c6ceda45b73f2deca24c29697a2014ce
HansFigo <hambasahaya0303@gmail.com>
2024-12-13 06:48:29 +0700

    add signal handlers to gracefully exit on SIGINT and SIGTERM

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,14 @@
 import type { Handle } from '@sveltejs/kit';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '$env/static/private';
+import process from 'process';
+
+process.on('SIGINT', () => {
+	process.exit();
+});
+process.on('SIGTERM', () => {
+	process.exit();
+});
 
 const handleAuth: Handle = async ({ event, resolve }) => {
 	const cookies = event.cookies.get('jwt');
